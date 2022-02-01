16d0c2fcb9b53fad740307c8778f4846bcf68bb5
dcat <dcatproject@yahoo.com>
2022-02-01 19:07:23 +0700

    add wikipedia

---
+++ add_on/scrap.js
@@ -0,0 +1,21 @@
+const wiki = require('wikijs').default;
+
+
+whatscmd.addCmd({
+    name: 'wiki',
+    hint: '.wiki panda',
+    desc: 'mencari artikel pada wikipedia'
+}, async (client, msg, args) => {
+    if (msg.noArgs) return msg.reply('apa yang mau dicari di wiki ?')
+    try {
+        var artikel = await wiki({
+                apiUrl: 'https://id.wikipedia.org/w/api.php'
+            })
+            .page(args);
+
+        var found = await artikel.rawContent();
+        msg.reply(found)
+    } catch (e) {
+        msg.reply('artikel tidak ditemukan!')
+    }
+})
--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-const ytdp = require('./add_on/ytdl_bin/ytdlp')
+const wiki = require('wikijs').default
 
 async function haha (){
-    const test = await ytdp('https://youtu.be/C_l_hBsy9mU')
-    console.log(test)
+   
 }
 
 haha()
